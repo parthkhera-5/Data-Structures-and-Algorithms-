74347c33e7c592e52049b8dee0ae6729288042f8
parthkhera-5 <parthkheraxa24@gmail.com>
2026-04-04 23:09:39 +0530

    Add files via upload

---
+++ String/CharString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class CharString{
+    public static void main(String[] args){
+    String s = "Hello everyone! This is a testing code";
+    for(int i=0; i<s.length(); i++){
+        System.out.print(s.charAt(i));
+    }        
+    } 
+}
