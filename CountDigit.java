7c4f6853f390d18d243143802fbb10c2c547a5a8
parthkhera-5 <parthkheraxa24@gmail.com>
2026-04-05 23:33:10 +0530

    Add files via upload

---
+++ CountDigit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class CountDigit {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+
+        String s = ""+n;
+        System.out.println(s.length());
+    }
+}
