4f8e00d6a74eda1eb029f84c9fc8d6af858b5935
parthkhera-5 <parthkheraxa24@gmail.com>
2026-04-05 23:35:02 +0530

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,14 @@
+public class Equals {
+    public static boolean equals(String s1, String s2){
+        if(s1.length()!=s2.length()) return false;
+        for(int i=0; i<s1.length(); i++){
+            if(s1.charAt(i)!=s2.charAt(i)) return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s1 = new String("raghav");
+        String s2 = new String("raghav"); 
+        System.out.println(s1.equals(s2));
+    }
+}
