07aad1d63b0496ef054a6af8185eddc01813f592
parthkhera-5 <parthkheraxa24@gmail.com>
2026-04-08 20:23:17 +0530

    Add files via upload

---
+++ String/compareTo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class compareTo {
+    // public static int compareToo(String a, String b){
+
+    // }
+    public static void main(String[] args) {
+        String a = "raghav";
+        String b = "preeti";
+        System.out.println(a.compareTo(b));
+        System.out.println(a.concat(b));
+    }
+}
+
+
+// 'r' is greater than 'p' by 2 that's why output is 2
+// "r" - "p" = 2 and "m" - "s" = -6
+// same string = 0
+// if str1= "preetam" and s2 = "preet" then -2, because the 2 characters are extra
