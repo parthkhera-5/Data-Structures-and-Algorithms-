98df9ded2bd684b3de9d08f2e65d56801462af80
parthkhera-5 <parthkheraxa24@gmail.com>
2026-04-13 23:48:14 +0530

    Add files via upload

---
+++ String/plusOp.java
@@ -0,0 +1,23 @@
+public class plusOp {
+    public static void main(String[] args) {
+        String a = "Bheem";
+        String b = "Kalia";
+        // System.out.println(a+b);
+        // a = a + " is a friend of raju";
+        // System.out.println(a);
+
+        // a+=10;
+        // System.out.println(a);
+
+        // a+='\n';
+        // a+='n';
+        // System.out.println(a);
+
+        // System.out.println("raghav"+10+20); (left to righ precedence)
+        // System.out.println(10+20+"raghav");
+        // System.out.println(10+"raghav"+20);
+        System.out.println("raghav"+(10+20));
+        System.out.println('A'+"harry");
+        System.out.println('A'+10+"Anant");
+    }    
+}
